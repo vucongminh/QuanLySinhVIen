4d9636aa03381fe8fc98d3d5b43896c0d8177590
vucongminh <36150774+vucongminh@users.noreply.github.com>
2018-03-08 13:32:33 +0700

    add
    hic

---
+++ officialver4/QuanLySinhVien/frmSuaLop.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLySinhVien
+{
+    public partial class frmSuaLop : Form
+    {
+        
+        string MaLop;
+        public frmSuaLop(string Ma)
+        {
+            
+            MaLop = Ma;
+            InitializeComponent();
+        }
+
+        private void frmSuaLop_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = KetNoi.str;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT TenLop FROM LOP WHERE MaLop='" + MaLop + "'";
+            SqlDataReader rd;
+            rd = cmd.ExecuteReader();
+            DataTable td = new DataTable();
+            td.Load(rd);
+            this.txtMaLop.Text = MaLop;
+            this.txtTenLop.Text = td.Rows[0][0].ToString();
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            frmDSLop frm = new frmDSLop();
+            frm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = KetNoi.str;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE LOP SET TenLop='" + txtTenLop.Text + "'WHERE MaLop='" + MaLop + "'";
+            DialogResult result;
+            result = MessageBox.Show("BẠN CÓ MUỐN THAY ĐỔI THÔNG TIN KHÔNG?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("CẬP NHẬT DỮ LIỆU THÀNH CÔNG", "THÔNG BÁO");
+                this.Close();
+                frmDSLop frm = new frmDSLop();
+                frm.Show();
+            }
+
+        }
+    }
+}
