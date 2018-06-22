5815e9128c43aeba54aae74539f79a6d51cc9665
vucongminh <36150774+vucongminh@users.noreply.github.com>
2018-06-22 15:23:14 +0700

    xong khach hang
    con` don hang

--- WebSiteBanSach/Controllers/QuanLyKhachHangController.cs
+++ WebSiteBanSach/Controllers/QuanLyKhachHangController.cs
@@ -36,5 +36,54 @@ namespace WebSiteBanSach.Controllers
             return View(db.KhachHangs.ToList().OrderBy(n => n.MaKH).ToPagedList(pageNumber, pageSize));
             // return View();
         }
+        // GET: KhachHang/Details/5
+        public ActionResult HienThi(int maKH)
+        {
+            KhachHang KhachHang = db.KhachHangs.SingleOrDefault(n => n.MaKH == maKH);
+            if (KhachHang == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            return View(KhachHang);
+            //return View();
+        }
+        // GET: KhachHang/Delete/5
+        public ActionResult Xoa(int maKH)
+        {
+            KhachHang KhachHang = db.KhachHangs.SingleOrDefault(n => n.MaKH == maKH);
+            if (KhachHang == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            return View(KhachHang);
+            //return View();
+        }
+
+        // POST: KhachHang/Delete/5
+        [HttpPost, ActionName("Xoa")]
+        public ActionResult XacNhanXoa(int maKH, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                KhachHang KhachHang = db.KhachHangs.SingleOrDefault(n => n.MaKH == maKH);
+                if (KhachHang == null)
+                {
+                    Response.StatusCode = 404;
+                    return null;
+                }
+                db.KhachHangs.Remove(KhachHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
